532c5e012399673f4c85b282c2fc8b1260b74403
ZIAN786 <zakirzian41@gmail.com>
2024-05-08 04:11:38 +0600

    manage restaurant front

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 
@@ -12,6 +13,7 @@ const AppRoutes = ()=> {
             <Route path = "/auth-callback" element = {<AuthCallbackPage/>} />
             <Route element={<ProtectedRoute />}>
             <Route path="/user-profile" element={<Layout> <UserProfilePage /> </Layout>} />
+            <Route path="/manage-restaurant" element={<Layout> <ManageRestaurantPage /> </Layout>}/>
             </Route>
             
             <Route path="*" element={<Navigate to="/" />} />  
