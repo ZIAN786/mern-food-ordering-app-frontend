5637dd98f862ee86a5455c17aa94125983011a71
ZIAN786 <zakirzian41@gmail.com>
2024-05-17 01:56:57 +0600

    order page & others completed

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 
@@ -13,6 +15,8 @@ const AppRoutes = ()=> {
             <Route path="/" element={<Layout><HomePage/></Layout>} />
             <Route path = "/auth-callback" element = {<AuthCallbackPage/>} />
             <Route path="/search/:city" element={<Layout showHero={false}> <SearchPage /> </Layout>}/>
+            <Route path="/detail/:restaurantId"element={<Layout showHero={false}> <DetailPage /> </Layout>}/>
+            <Route path="/order-status" element={<Layout> <OrderStatusPage /> </Layout>}/>
             <Route element={<ProtectedRoute />}>
             <Route path="/user-profile" element={<Layout> <UserProfilePage /> </Layout>} />
             <Route path="/manage-restaurant" element={<Layout> <ManageRestaurantPage /> </Layout>}/>
